ec63ea52ddf27b619f270fe91c6f9c423fcc5edc
ionica21 <ionicat@windowslive.com>
2023-08-31 12:25:21 +0200

    Rebuild lib

--- lib/factory/factory.d.ts
+++ lib/factory/factory.d.ts
@@ -1,2 +1,5 @@
 import { IDict, IPoolData, ICurve } from "../interfaces";
+export declare const BLACK_LIST: {
+    [index: number]: any;
+};
 export declare function getFactoryPoolData(this: ICurve, fromIdx?: number, swapAddress?: string, factoryAddress?: string): Promise<IDict<IPoolData>>;
